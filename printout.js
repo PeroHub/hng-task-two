94e5a821e110d0a01c6ebe64e27a3fd91708bda4
PeroHub <peterime600@gmail.com>
2021-08-21 13:34:51 +0100

    features

--- printout.js
+++ printout.js
@@ -4,14 +4,15 @@ let fullName = document.getElementById("name")
 
 
 const logout = (value1, value2) => {
+    fullName.classList.add("type")
     return fullName.textContent = `${value1}  ${value2}`
 }
 
 
 // Object of first and last name
 const nameObject =  {
-    fName: "Peter",
-    lname: "Ime"
+    fName: "I Am Peter",
+    lname: "Ime."
 }
 
 const final = logout(nameObject.fName, nameObject.lname)
